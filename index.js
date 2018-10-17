05eba514c5268c87d7a8d2bf9a10fc18c940d98f
Orlando Leite <orlleite@gmail.com>
2018-10-17 13:10:35 -0400

    Fixed indentation

--- index.js
+++ index.js
@@ -6,9 +6,9 @@
 
 Object.defineProperty(String.prototype, 'cleanSpecialChars',
 {
-	enumerable: false,
-	value: function ()
-	{
-		return this.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/[^a-zA-Z0-9 ]/g,'_');
-	}
+    enumerable: false,
+    value: function ()
+    {
+        return this.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/[^a-zA-Z0-9 ]/g,'_');
+    }
 });
