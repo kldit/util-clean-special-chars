173348fd6b3e75d12910b19c69268dd0e93754e2
Orlando Leite <orlleite@gmail.com>
2018-10-25 18:54:22 -0300

    Fixed function already exists

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
  *
  * util functions
  */
-
-Object.defineProperty(String.prototype, 'cleanSpecialChars',
+if(!String.prototype.cleanSpecialChars)
 {
-    enumerable: false,
-    value: function ()
+    Object.defineProperty(String.prototype, 'cleanSpecialChars',
     {
-        return this.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/[^a-zA-Z0-9 ]/g,'_');
-    }
-});
+        enumerable: false,
+        value: function ()
+        {
+            return this.normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/[^a-zA-Z0-9 ]/g, '_');
+        }
+    });
+}
